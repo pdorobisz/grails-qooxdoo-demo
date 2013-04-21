535f8cbebddeb7e617a4ad61b5bdf71ccfdba89c
Piotr Dorobisz <pdorobisz@gmail.com>
2013-04-21 11:24:57 +0200

    configure grails to build qooxdoo application when packaging war file

--- scripts/_Events.groovy
+++ scripts/_Events.groovy
@@ -1,4 +1,23 @@
+import grails.util.Environment
+
+import java.nio.file.Paths
+
 includeTargets << grailsScript("_GrailsEvents")
+
+final def APP_DIR = "qooxdoo"
+final def SRC_DIR = "$APP_DIR/build"
+final def TARGET_DIR = "web-app/qx"
+
+eventPackagingEnd = { warName ->
+    if (Environment.getCurrent() == Environment.PRODUCTION) {
+        def qooxdooPath = config.qooxdoo.sdk.path
+        def generatorPath = Paths.get(qooxdooPath, "tool/bin/generator.py")
+        AntBuilder ant = new AntBuilder()
+        ant.exec(dir: APP_DIR, executable: "python") { arg(line: "-u $generatorPath -m QOOXDOO_PATH:$qooxdooPath build") }
+        ant.delete(dir: TARGET_DIR)
+        ant.move(todir: TARGET_DIR) { fileset(dir: SRC_DIR, excludes: "index.html") }
+    }
+}
 
 eventCompileEnd = {
     ant.copy(file: "${basedir}/grails-app/conf/User-config.groovy", todir: classesDirPath)
